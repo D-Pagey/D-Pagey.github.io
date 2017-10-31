ff113bd25afdb8768d38717991658d3b7868b11f
D-Pagey <dan.page91@gmail.com>
2017-10-31 18:43:47 +0000

    Added links for contact section. Updated thumbnail.

--- scripts.js
+++ scripts.js
@@ -1,41 +1,51 @@
 /* IDEAS:
-  1) Replace functionality with JS
-  2) Click top image cycles through array of images
+  Click top image cycles through array of images
 */
 
-// Click thumbnail to CodePen demo
+// Constants
 const project1El = document.getElementById("project1");
 const project2El = document.getElementById("project2");
 const project3El = document.getElementById("project3");
 const project4El = document.getElementById("project4");
 
-function project(name, url) {
+// Link object constructor
+function link(name, url) {
   this.name = name;
   this.url = url;
 }
 
-const project1 = new project(project1El.alt, "https://codepen.io/Pagey/full/GmOYdm/");
-const project2 = new project(project2El.alt, "https://codepen.io/Pagey/full/eEXRZy/");
-const project3 = new project(project3El.alt, "https://codepen.io/Pagey/full/wrZRqR/");
-const project4 = new project(project4El.alt, "https://codepen.io/Pagey/full/GMgrwM/");
+const project1 = new link(project1El.alt, "https://codepen.io/Pagey/full/GmOYdm/");
+const project2 = new link(project2El.alt, "https://codepen.io/Pagey/full/eEXRZy/");
+const project3 = new link(project3El.alt, "https://codepen.io/Pagey/full/wrZRqR/");
+const project4 = new link(project4El.alt, "https://codepen.io/Pagey/full/GMgrwM/");
+const github = new link("GitHub Page", "https://github.com/D-Pagey");
+const twitter = new link("Twitter Page", "https://twitter.com/Pagey64")
 
-// Open CodePen Demo
-function liveDemo(id) {
+// Open link
+function openLink(id) {
     window.open(id.url);
 }
 
 project1El.addEventListener("click", function() {
-  liveDemo(project1)
+  openLink(project1)
 });
 
 project2El.addEventListener("click", function() {
-  liveDemo(project2)
+  openLink(project2)
 });
 
 project3El.addEventListener("click", function() {
-  liveDemo(project3)
+  openLink(project3)
 });
 
 project4El.addEventListener("click", function() {
-  liveDemo(project4)
+  openLink(project4)
+});
+
+document.getElementById("github").addEventListener("click", function() {
+  openLink(github)
+});
+
+document.getElementById("twitter").addEventListener("click", function() {
+  openLink(twitter)
 });
