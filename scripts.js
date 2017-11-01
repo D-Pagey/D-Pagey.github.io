9a881126689780ff7a941ad665c99a4e1e923d41
D-Pagey <dan.page91@gmail.com>
2017-11-01 00:36:26 +0000

    Added slideshow.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
-/* IDEAS:
-  Click top image cycles through array of images
-*/
+// Click slideshow for mainImg
+const images = [
+  "img/dive.jpg",
+  "img/africa.jpg",
+  "img/topcats.jpg",
+  "img/castle.jpg",
+  "img/paintball.jpg"
+];
+let myIndex = 4;
+const headlineImg = document.getElementById("mainImg");
+headlineImg.src = images[myIndex];
+
+headlineImg.addEventListener("click", function() {
+  myIndex = (myIndex+1)%(images.length);
+  headlineImg.src = images[myIndex];
+})
 
 // Constants
 const project1El = document.getElementById("project1");
