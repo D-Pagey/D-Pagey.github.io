6a8cd4166f56edcca3d2fce4f49fe4110473c9c5
D-Pagey <dan.page91@gmail.com>
2018-06-10 19:12:24 +0100

    Styling for iPad.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,11 +8,13 @@ function Header() {
         <header className='header'>
             <img src={heroImg} alt='Dan with dogs' 
             className='main-img'/>
-            <h1 className='title'>Dan Page</h1>
-            <p className='intro-copy'>I'm a self taught Front End Developer 
-            focussing on building apps in React.</p>
-            <p className='intro-copy'>Below is a selection of the 10+ projects I
-             have built in Vanilla JS and then rebuilt in React.</p> 
+            <div className='header-text'>
+                <h1 className='title'>Dan Page</h1>
+                <p className='intro-copy'>I'm a self taught Front End Developer 
+                focussing on building apps in React.</p>
+                <p className='intro-copy'>Below is a selection of the 10+ projects I
+                 have built in Vanilla JS and then rebuilt in React.</p> 
+            </div>
         </header>
     )
 }
